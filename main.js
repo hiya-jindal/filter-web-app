903cffb315cbc897b1947dffc8f9bec9865de158
hiya-jindal <137200203+hiya-jindal@users.noreply.github.com>
2024-01-24 18:00:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -30,7 +33,16 @@ function gotPoses(results)
     if(results.length > 0 )
     {
         console.log(results);
-        console.log("nose x = "+ results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = " + noseY);
     }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
